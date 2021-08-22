95a15cc02c85db1c72044af9e2f627f45d90842d
Elad <eladridel@gmail.com>
2021-08-22 19:14:15 +0300

    4

--- app/src/views/screens/HomeScreen.js
+++ app/src/views/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import firebase from "firebase";
 class HomeScreen extends Component {
   state = {};
   render() {
     return (
       <View>
         <Text>Home</Text>
+        <Button
+          title="sign out"
+          onPress={() => firebase.auth().signOut()}
+        ></Button>
       </View>
     );
   }
